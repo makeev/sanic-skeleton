4ea4010230abf704bc4f5464bbcfc9894a383814
Mikhail Makeev <mihail.makeev@gmail.com>
2021-06-02 09:08:17 +0300

    sanic version upd

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ def register_redis(app: Sanic, redis_url, minsize=5, maxsize=10) -> None:
     @app.listener("before_server_start")
     async def init_redis(app, loop):
         try:
-            app.redis =  await aioredis.create_redis_pool(
+            app.ctx.redis =  await aioredis.create_redis_pool(
                 redis_url, minsize=minsize, maxsize=maxsize, loop=loop
             )
             logger.info("Redis pool created")
@@ -20,8 +20,8 @@ def register_redis(app: Sanic, redis_url, minsize=5, maxsize=10) -> None:
     @app.listener("after_server_stop")  # type: ignore
     async def close_orm(app, loop):  # pylint: disable=W0612
         if hasattr(app, 'redis'):
-            app.redis.close()
-            await app.redis.wait_closed()
+            app.ctx.redis.close()
+            await app.ctx.redis.wait_closed()
             logger.info('redis closed')
 
 
